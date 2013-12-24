b8e6895087bc0ea53af83b1ba21965a04d168426
silps <silver.kuusik@gmail.com>
2013-12-24 01:07:51 +0100

    removed obsolete fields

--- volunteers/forms.py
+++ volunteers/forms.py
@@ -1,7 +1,40 @@
+from volunteers.models import VolunteerTask, TaskCategory, Edition, Task
+
 from django import forms
-from volunteers.models import VolunteerTask
-from django.forms.extras.widgets import SelectDateWidget
 from django.contrib.admin import widgets
+from django.utils.translation import ugettext as _
+from django.forms.extras.widgets import SelectDateWidget
+
+from userena.utils import get_profile_model
 
 class AddTasks(forms.Form):
     tasks = forms.ModelMultipleChoiceField(queryset=VolunteerTask.objects.none(), widget=forms.CheckboxSelectMultiple())
+
+class EditProfileForm(forms.ModelForm):
+    """ Base form used for fields that are always required """
+    first_name = forms.CharField(label=_(u'First name'), max_length=30, required=False)
+    last_name = forms.CharField(label=_(u'Last name'), max_length=30, required=False)
+
+    categories = forms.ModelMultipleChoiceField(label=_(u'Categories'), queryset=TaskCategory.objects.all(), widget=forms.CheckboxSelectMultiple(), required=False)
+
+    def __init__(self, *args, **kw):
+        super(EditProfileForm, self).__init__(*args, **kw)
+        # Put the first and last name at the top
+        new_order = self.fields.keyOrder[:-2]
+        new_order.insert(0, 'first_name')
+        new_order.insert(1, 'last_name')
+        self.fields.keyOrder = new_order
+
+    class Meta:
+        model = get_profile_model()
+        exclude = ['user', 'editions', 'tasks']
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        profile = super(EditProfileForm, self).save(commit=commit)
+        # Save first and last name
+        user = profile.user
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+
+        return profile
