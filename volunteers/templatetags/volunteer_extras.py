60c1c1d36066b967ef32e3474f40fe2c037b73fc
silps <silver.kuusik@gmail.com>
2014-01-07 07:09:42 +0100

    added temaplate tag

---
+++ volunteers/templatetags/volunteer_extras.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_item(container, key):
+    if type(container) is dict:
+        return container.get(key)
+    elif type(container) in (list, tuple):
+        return container[key] if len(container) > key else None
+    return None
