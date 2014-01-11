d2927716453197e6c6c3b1175350b40e4c84d1f0
silps <silver.kuusik@gmail.com>
2014-01-11 16:15:46 +0100

    added static and media paths

--- volunteer_mgmt/settings.py
+++ volunteer_mgmt/settings.py
@@ -4,7 +4,6 @@ import os
 import dj_database_url
 
 settings_dir = os.path.dirname(__file__)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(settings_dir))
 
 DEBUG = True
@@ -34,20 +33,20 @@ else:
     }
 
 # Django Userena configs
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('SMTP_USERNAME')
 EMAIL_HOST_PASSWORD = os.environ.get('SMTP_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('SMTP_FROM_EMAIL')
 
 SITE_ID = 1
 ANONYMOUS_USER_ID = -1
-AUTH_PROFILE_MODULE = 'volunteers.Volunteer'
-USERENA_SIGNIN_REDIRECT_URL = '/volunteers/%(username)s/'
-USERENA_DISABLE_PROFILE_LIST = False
 USERENA_MUGSHOT_SIZE = 140
+USERENA_DISABLE_PROFILE_LIST = False
+AUTH_PROFILE_MODULE = 'volunteers.Volunteer'
 TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
+USERENA_SIGNIN_REDIRECT_URL = '/volunteers/%(username)s/'
 
 _ = lambda s: s
 LANGUAGES = (
@@ -77,8 +76,6 @@ TIME_ZONE = 'Europe/Brussels'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
@@ -92,18 +89,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'public/media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'public/static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -115,7 +112,6 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(PROJECT_ROOT, 'volunteers/static/'),
-    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -127,7 +123,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '2#kb-z%w)=+gig=_&&za2*+5#gyc369kf$i+7n^l($#*rr=o5n'
+SECRET_KEY = os.environ.get('SESSION_SECRET')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -162,7 +158,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'volunteers/templates/'),
+    os.path.join(PROJECT_ROOT, 'templates/'),
 )
 
 INSTALLED_APPS = (
