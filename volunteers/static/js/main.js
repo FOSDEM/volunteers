238d82be7cb015c9a954b15c49e3b6a3d2d58f8e
silps <silver.kuusik@gmail.com>
2014-01-12 16:35:45 +0100

    updated dialog

--- volunteers/static/js/main.js
+++ volunteers/static/js/main.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-	$( document ).tooltip({
-		items: "img, [data-geo], [title]",
+	var temp_title = "";
+	$("#dialog").dialog({ autoOpen: false });
+	$(".task-name").click(function() {
+		$("#dialog").html(temp_title);
+		$("#dialog").dialog("open");
+	});
+	$(".task-volunteers").click(function() {
+		$("#dialog").html(temp_title);
+		$("#dialog").dialog("open");
+	});
+	$(".task_list").tooltip({
+		items: "[title]",
 		content: function() {
 			var element = $(this);
 			if ( element.is("[title]") ) {
-				return element.attr("title");
+				temp_title = element.attr("title");
+				return temp_title;
 			}
 		}
 	});
