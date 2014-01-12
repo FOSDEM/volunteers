95ea8eca93cab10764d7ec352fa85305bcf0f7d8
silps <silver.kuusik@gmail.com>
2014-01-12 13:22:57 +0100

    added <br> capabilty into title

--- volunteers/static/js/main.js
+++ volunteers/static/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	$(document).tooltip();
+	$( document ).tooltip({
+		items: "img, [data-geo], [title]",
+		content: function() {
+			var element = $(this);
+			if ( element.is("[title]") ) {
+				return element.attr("title");
+			}
+		}
+	});
 });
