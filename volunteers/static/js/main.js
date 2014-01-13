8d9c5aba1b484cc9b49307c10bf78b597b90c76b
silps <silver.kuusik@gmail.com>
2014-01-13 20:09:54 +0100

    updated main.js added talk volunteers dialog

--- volunteers/static/js/main.js
+++ volunteers/static/js/main.js
@@ -9,7 +9,21 @@ $(document).ready(function() {
 		$("#dialog").html(temp_title);
 		$("#dialog").dialog("open");
 	});
+	$(".talk-volunteers").click(function() {
+		$("#dialog").html(temp_title);
+		$("#dialog").dialog("open");
+	});
 	$(".task_list").tooltip({
+		items: "[title]",
+		content: function() {
+			var element = $(this);
+			if ( element.is("[title]") ) {
+				temp_title = element.attr("title");
+				return temp_title;
+			}
+		}
+	});
+	$(".talk_list").tooltip({
 		items: "[title]",
 		content: function() {
 			var element = $(this);
