17428a6f7cb4e115f5f8ebc5f2165838c992409e
Silver Kuusik <silver.kuusik@gmail.com>
2015-01-08 15:51:15 +0100

    updated responsive design

--- volunteers/static/js/main.js
+++ volunteers/static/js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-	var div = document.getElementById("container");
-	div.innerHTML = div.innerHTML + "<br>screen width: " + document.documentElement.clientWidth;
 	var temp_title = "";
 	$("#dialog").dialog({ autoOpen: false });
 	/*$(".task-name").click(function() {
@@ -35,4 +33,36 @@ $(document).ready(function() {
 			}
 		}
 	});
+
+	/* debug the screen width */
+	var div = document.getElementById("container");
+	div.innerHTML = div.innerHTML + "<br>screen width: " + document.documentElement.clientWidth;
+	/* for mobile browsing */
+	if ( document.documentElement.clientWidth <= 1000 ) {
+		/* collapse and open menu */
+		$("span.menu-button").on('click', function() {
+			var elem = $('#nav1');
+			if (elem.is(":visible")) elem.hide('fast');
+			else elem.show('fast');
+		});
+		/* collapse tasks */
+		$("table.task_list tbody tr:not(.category)").hide();
+		/* open tasks when clicked */
+		$("table tbody tr.category").on('click', function() {
+			$(this).nextUntil(".row-hide").each(function() {
+				if ($(this).is(":visible")) {
+					$(this).hide('fast');
+				} else {
+					$(this).show('fast');
+					$(this).css('display', 'block');
+				}
+			});
+		});
+		/* open talks when clicked */
+		$("legend.talk_list").on('click', function() {
+			var elem = $(this).next();
+			if (elem.is(":visible")) elem.hide('fast');
+			else elem.show('fast');
+		});
+	}
 });
