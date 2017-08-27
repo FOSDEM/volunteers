19ecc9483f87f04803f830ac15dfa2d5ccb6c685
Juan Rial <jrial@safehex.be>
2017-08-27 04:14:08 +0200

    Settings cleanup and improvements
    * Split settings into base and local
    * Remove dependency on environment variables
    * Move into settings folder
    * Better SECRET_KEY handling

--- volunteer_mgmt/settings/local_example.py
+++ volunteer_mgmt/settings/local_example.py
@@ -1,9 +1,18 @@
+import dj_database_url
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
 # Admins
 ADMINS = (
     ('Your Name', 'your_email@example.com'),
 )
 MANAGERS = ADMINS
 
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+# Allow all host headers
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Database config
 # When on heroku
@@ -29,13 +38,23 @@ else:
         }
     }
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'demo'
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'Europe/Brussels'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
 
 # Volunteer sync
 # Currently we only support petabarf.
 SCHEDULE_SYNC_TYPE = 'pentabarf'
 SCHEDULE_SYNC_URI = 'https://example.org/schedule/xml'
+
+ROOMS_HERALDING_REQUIRED = ['Janson', 'K.1.105 (La Fontaine)']
+ROOMS_VIDEO_NEEDED = ['Janson', 'K.1.105 (La Fontaine)']
 
 # Userena settings
 EMAIL_PORT = 25
