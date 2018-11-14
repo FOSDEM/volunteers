f44cc48681fc870009436820342643dc632e2b1a
FOSDEM Volunteer Administrators <volunteers-admin@fosdem.org>
2018-11-14 21:48:49 +0100

    Added deployment instructions

---
+++ deployment/playbook/README.md
@@ -0,0 +1,11 @@
+# Deploy fosdem-volunteers
+
+## Prerequisites
+* A system with SystemD is required.
+* An account that can either switch to root via sudo or su; and to another user, is required.
+
+## Instructions
+* Run the provided playbook with the correct inventory.
+
+## Manual actions
+* A `localsettings.py` file must still be created. Copy the provided `localsettings_example.py` file and modify it according to your needs.
---
+++ volunteer_mgmt/localsettings_example.py
@@ -0,0 +1,47 @@
+TEMPLATE_DEBUG = True
+USE_X_FORWARDED_HOST = True
+
+# Admins
+MANAGERS = ADMINS = (
+    ('Me', 'me@fosdem.org')
+)
+
+
+# Database config
+# When on heroku
+if 'dj_database_url' in locals() and dj_database_url.config():
+    DATABASES = {}
+    DATABASES['default'] = dj_database_url.config()
+# When on localhost
+else:
+    DATABASES = {
+        'default': {
+            # 'django.db.backends.' +  'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            # Or path to database file if using sqlite3.
+            'NAME': 'volunteers',
+            'USER': 'volunteers',
+            'PASSWORD': 'volunteers',
+            'HOST': '127.0.0.1'
+            # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+            # Set to empty string for default.
+            # 'PORT': '5432',
+        }
+    }
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'VERY_SECRET'
+
+# Volunteer sync
+# Currently we only support petabarf.
+SCHEDULE_SYNC_TYPE = 'pentabarf'
+SCHEDULE_SYNC_URI = 'https://fosdem.org/schedule/xml'
+
+# Userena settings
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
+EMAIL_HOST = '127.0.0.1'
+DEFAULT_FROM_EMAIL = 'volunteer-admin@lists.fosdem.org'
+
+SETUP_FOR_CURRENT_YEAR_COMPLETE = True
+IMPORT_VIDEO_TASKS = False
